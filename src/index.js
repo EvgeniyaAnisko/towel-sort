d14d45c05946a7bf13d30b782172b27a7b4a4c18
EvgeniyaAnisko <evgeniya.anisko@gmail.com>
2022-01-26 23:46:07 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 
 // You should implement your task here.
 
+function flatten(array) {
+  var flattend = [];
+  let element = []
+  for (let i = 0; i < array.length; i++) {
+    if (i%2)
+      element = array[i].reverse();
+    else
+      element = array[i]
+    flattend.push(element)
+  }
+  return flattend.flat();
+}
+
+
 module.exports = function towelSort (matrix) {
-  return [];
+  if (!matrix)
+    return []
+  return flatten(matrix);
 }
